939fc72301e85c450b57482761613002d391e0f4
Joshua Butler <sentinel7719@protonmail.com>
2025-01-17 18:58:10 -0600

    Class-based tests, Fixtures, Mark And Parameterize, Mocking

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,5 +1,6 @@
 import pytest
 import source.my_functions as my_functions
+import time
 
 def test_add():
     result = my_functions.add(number_one=1, number_two=4)
@@ -16,4 +17,18 @@ def test_divide_by_zero():
 def test_add_strings():
     result = my_functions.add(number_one="I like ", number_two="burgers")
     assert result == "I like burgers"
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(number_one=10, number_two=5)
+    assert result == 2
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    assert my_functions.add(1, 2) == 3
+
+@pytest.mark.xfail(reason="We know we cannot divide by zero")
+def test_divide_zero_broken():
+    my_functions.divide(4, 0)
 
